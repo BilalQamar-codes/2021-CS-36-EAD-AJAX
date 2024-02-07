4adcd78a4545a3106bd25729f8f3f8f560487db1
QuantamCoder3941 <=>
2024-02-08 01:37:36 +0500

    Complete

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ function getData() {
         xhr.onload = function () {
             if (xhr.status === 200)
             {
+                // updating the object
                 let obj = JSON.parse(this.responseText);
                 Response.city = obj.name;
                 Response.country = obj.sys.country;
@@ -40,10 +41,25 @@ function getData() {
                 Response.des = obj.weather[0].description;
                 Response.humidity = obj.main.humidity;
                 Response.wind = obj.wind.speed;
+                console.log(Response.city+Response.country);
+                console.log(document.getElementById("detail").innerHTML)
                 
+                // changing all the fields according
+                document.getElementById("detail").innerHTML = Response.city+","+Response.country;
+                document.getElementById("temp").innerHTML = "Temprature:   "+Response.temp+" C";
+                document.getElementById("feels").innerHTML = "Feels-Like:   "+Response.feels + " C";
+                document.getElementById("des").innerHTML = "Description:   "+Response.des;
+                document.getElementById("humidity").innerHTML = "Humidity:   "+Response.humidity+" %";
+                document.getElementById("wind").innerHTML = "Wind-Speed:   "+Response.wind+" m/s";
+                var iconurl = "http://openweathermap.org/img/w/" + Response.icon + ".png";
+                document.getElementById("icon").src= iconurl;
+            }
+            else{
+                alert("Not Found!!!!!1");
             }
         }
         // send request
+        
         xhr.send();
     }
 
